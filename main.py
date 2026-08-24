2b0ebb0ab37836e0dfbefcb2babdca0b8d4f0d12
abdulborimusaev0122-hub <abdulborimusaev0122@gmail.com>
2026-08-25 01:58:40 +0500

    Fix typo in TOKEN variable assignment

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import telebot
 from telebot import types
 import psycopg2
 
-TOKEN = os.environ.get("8957204394:AAFasf98ogRUBD4zYnyszlaCor_-3FkXwlw")
+TюTOKEN = os.environ.get("8957204394:AAFasf98ogRUBD4zYnyszlaCor_-3FkXwlw")
 DATABASE_URL = os.environ.get("DATABASE_URL")
 WEB_APP_URL = "https://abdulborimusaev0122-hub.github.io/tictactoe/"
 
